080690ba8b2dd2e24873bad942fb997d1d9cbca6
Rob Pickard <rob.pickard11@gmail.com>
2019-11-25 22:33:55 +0000

    Further improvements to models and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,7 +27,7 @@ france = Country.new(
 france.save!
 
 
-puts "Filling countries with resorts"
+puts "Creating resorts"
 courchavel = Resort.new(
   name: "Courchavel",
   description: "Courchevel is a winter playground for the rich and famous – attracting a steady influx of stars from Beyonce to Beckham. Ironically, the resort was originally planned to create jobs and skiing for the masses, but somewhere along the lines it morphed into the luxury resort of The Alps.\nThere are 50 five-star hotels in the whole of France and nine of them are in Courchevel. As well as top hotels, the resort now boasts three Michelin-star restaurants. The multi-level resort, complete with heated pavements, is dotted with more than 100 boutiques - you’ll find as many diamond dealers here as ski-hire shops. The tourist board does stress they offer a range of amenities for a wider clientele; it’s not all Hermès, Louis Vuitton and Valentino.\nThe resort consists of four ski villages: Courchevel 1300 (La Praz), Courchevel 1550, Courchevel 1650 and Courchevel 1850. While 1850 has the most high-end hotels and shops, its modern development is decidedly lacking in charm. Fans of the traditional French feel should head to Courchevel 1650.\nCourchevel is a good all-round resort. Its ski area offers a good selection of runs for all levels and it is also popular with families, off-pisters and non-skiers. The resort’s 150 kilometres is enough for most, but the avid skier can buy a pass to the vast Three Valleys ski area, opening up 600 kilometres of lift-linked runs. Ski Pass for access to the vast lift-linked ski area in the world (600km).",
@@ -68,21 +68,20 @@ user_infos.each do |user_info|
 end
 
 
-puts "Filling resorts with squads and users!"
+puts "Filling resorts with squads and users"
 group_names = [["Freestylers", "Hardcore freestylers get involved!"],
   ["Big birthday drinks", "Open to everyone! Come and join"],
   ["Off-piste buddies", "Looking for some squaddies to come off-piste for a few days!"]]
-group_names.each do |group_name|
-  group = Group.new(name: group_name[0], description: group_name[1])
-  resorts.each do |resort|
-    group.resort = resort
-  end
-  group.save!
-
-  users.each do |user|
-    membership = Membership.new(user: user, group: group)
-    membership.save!
-  end
+resorts.each do |resort|
+    group_names.each do |group_name|
+      group = Group.new(name: group_name[0], description: group_name[1])
+      group.resort = resort
+      group.save!
+        users.each do |user|
+          membership = Membership.new(user: user, group: group)
+          membership.save!
+        end
+    end
 end
 
 
