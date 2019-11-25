f6a19994bb1916f9ceb3ca4263809d6168e023c0
Rob Pickard <rob.pickard11@gmail.com>
2019-11-25 18:12:26 +0000

    Ready for review tomorrow

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
 puts "Clearing records"
 Message.destroy_all
 Membership.destroy_all
@@ -12,6 +14,8 @@ User.destroy_all
 Group.destroy_all
 Resort.destroy_all
 Country.destroy_all
+
+
 
 puts "Creating countries"
 france = Country.new(
@@ -32,6 +36,37 @@ courchavel = Resort.new(
 courchavel.country = france
 courchavel.save!
 
+vdi = Resort.new(
+  name: "Val d'Isère",
+  description: "This world-class resort has earned its reputation as one of France’s top ski destinations. Val d’Isère has something for skiers and boarders of all levels: Olympic and World Cup runs, a wealth of fantastic off-piste opportunities and a varied selection of pistes including greens high up on the mountain.\nThe base of Val d'Isère sits at 1850 metres and from there, the pistes climb up to 3488 metres, at the Pointe du Montet and the Pissaillas glacier, just below at 3450 metres. Skiers have a choice of 150 kilometres of pistes for all levels, serviced by 90 lifts.\nVal d'Isere is part of the Espace Killy ski area, shared with Tignes. Some of the world's best skiers leave their tracks here and the resort has hosted Olympic and World Cup downhill competitions, as well as the Alpine Ski World Championships.",
+  address: "VAL D'ISÈRE, B.P. 228, 73150 VAL D'ISERE, France"
+  )
+vdi.country = france
+vdi.save!
+
+tignes = Resort.new(
+  name: "Tignes",
+  description: "At Tignes, you can ski before and after everyone else. That’s the motto of this large resort, which offers summer glacier skiing and a winter season stretching from September to May. With 150 kilometres of runs and such a long season, Tignes really does have something for everyone, but intermediate and advanced skiers are especially served. Freestylers and riders will also feel at home, since Tignes was one of France’s first resorts to promote alternative ski culture and continues to welcome it with open arms.",
+  address: "TIGNES, B.P. 51, 73320 TIGNES Cedex, France"
+  )
+tignes.country = france
+tignes.save!
+resorts = [courchavel, vdi, tignes]
+
+puts "Creating users"
+users = []
+user_infos = [["Rob", "Pickard", "rob@skisquad.com", "123456", URI.open('https://res.cloudinary.com/dr48k5zwm/image/upload/v1574331235/bdLSqWADjpU2a5cT7vbDGKqn.jpg')],
+              ["Ben", "Mclaren", "ben@skisquad.com", "123456", URI.open('https://res.cloudinary.com/dr48k5zwm/image/upload/v1574332988/47673163_nfnpke.jpg')],
+              ["Ross", "Bickmore", "ross@skisquad.com", "123456", URI.open('https://res.cloudinary.com/dr48k5zwm/image/upload/v1574332864/T02NE0241-UNR7LT7EW-067763db4da5-512_b1ex8x.jpg')],
+              ["Olivier", "Deman", "olivier@skisquad.com", "123456", URI.open('https://res.cloudinary.com/dr48k5zwm/image/upload/v1574331786/CyY16fryq1JUFW1ewWzBxyDG.jpg')]]
+user_infos.each do |user_info|
+  user = User.new(first_name: user_info[0], last_name: user_info[1], email: user_info[2], password: user_info[3])
+  user.resort = courchavel
+  user.photo.attach(io: user_info[4], filename: "profile_picture_#{user_info[0]}.jpg", content_type: 'image/jpg')
+  user.save!
+  users << user
+end
+
 
 puts "Filling resorts with squads and users!"
 group_names = [["Freestylers", "Hardcore freestylers get involved!"],
@@ -39,19 +74,12 @@ group_names = [["Freestylers", "Hardcore freestylers get involved!"],
   ["Off-piste buddies", "Looking for some squaddies to come off-piste for a few days!"]]
 group_names.each do |group_name|
   group = Group.new(name: group_name[0], description: group_name[1])
-  group.resort = courchavel
+  resorts.each do |resort|
+    group.resort = resort
+  end
   group.save!
 
-  user_infos = [["Rob", "Pickard", "rob@skisquad.com", "123456", URI.open('https://res.cloudinary.com/dr48k5zwm/image/upload/v1574331235/bdLSqWADjpU2a5cT7vbDGKqn.jpg')],
-    ["Ben", "Mclaren", "ben@skisquad.com", "123456", URI.open('https://res.cloudinary.com/dr48k5zwm/image/upload/v1574332988/47673163_nfnpke.jpg')],
-    ["Ross", "Bickmore", "ross@skisquad.com", "123456", URI.open('https://res.cloudinary.com/dr48k5zwm/image/upload/v1574332864/T02NE0241-UNR7LT7EW-067763db4da5-512_b1ex8x.jpg')],
-    ["Olivier", "Deman", "olivier@skisquad.com", "123456", URI.open('https://res.cloudinary.com/dr48k5zwm/image/upload/v1574331786/CyY16fryq1JUFW1ewWzBxyDG.jpg')]]
-  user_infos.each do |user_info|
-    user = User.new(first_name: user_infos[0], last_name: user_infos[1], email: user_infos[2], password: user_infos[3])
-    user.resort = courchavel
-    user.photo.attach(io: user_infos[4], filename: "profile_picture_#{user_infos[0]}.jpg", content_type: 'image/jpg')
-    user.save!
-
+  users.each do |user|
     membership = Membership.new(user: user, group: group)
     membership.save!
   end
