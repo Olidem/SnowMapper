914e6a58dddd54986625f000aef84737041427ed
Rob Pickard <rob.pickard11@gmail.com>
2019-11-26 10:59:03 +0000

    Revert "Devise registration controller edited to allow setting of resort"
    This reverts commit 068bd90a36c51a155de95693c9a9642448dab8b1.

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -1,17 +1,10 @@
 class ResortsController < ApplicationController
-  before_action :set_resort, only: %i[show]
-
   def index
     @resorts = Resort.all
   end
 
   def show
-    @groups = @resort.groups
-  end
-
-  private
-
-  def set_resort
     @resort = Resort.find(params[:id])
+    @groups = @resort.groups
   end
 end
