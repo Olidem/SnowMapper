ef13c7901d866ef9e3d1fe34d958596e912969cc
Ben McLaren <benmclaren@live.co.uk>
2019-11-26 18:04:31 +0000

    index page first design

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,18 @@
 import "bootstrap";
 import $ from 'jquery'
 import 'slick-carousel'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
+// import 'slick-carousel/slick/slick.css'
+// import 'slick-carousel/slick/slick-theme.css'
 
-$(document).ready(function(){
-  // $('.country-carousel').slick({
-  // });
+// $(document).ready(function(){
+//   // $('.country-carousel').slick({
+//   // });
 
-  // $('.country-carousel').slick();
-  $('.country-carousel').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+//   // $('.country-carousel').slick();
+//   $('.country-carousel').slick({
+//     infinite: true,
+//     slidesToShow: 3,
+//     slidesToScroll: 3
+//   });
 
-});
+// });
