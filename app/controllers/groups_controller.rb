36623540aa541191f00ca98514e82ce2522ed5e0
ROSS BICKMORE <rossbickmore@gmail.com>
2019-11-26 10:56:05 +0000

    added messages controller but experienced problems with ajax

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,11 @@
 class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+    @message = Message.new
+    @user = current_user
+  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,43 @@
 class MessagesController < ApplicationController
+  def index
+  end
+
+  def show
+
+  end
+
+  def new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @message = Message.new(message_params)
+    @user = current_user
+    @message.user = @user
+    @message.group = @group
+    if @message.save
+      respond_to do |format|
+        format.html { redirect_to group_path(@group) }
+        format.js  # <-- will render `app/views/reviews/create.js.erb`
+      end
+    else
+      respond_to do |format|
+        format.html { render 'groups/show' }
+        format.js  # <-- idem
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
