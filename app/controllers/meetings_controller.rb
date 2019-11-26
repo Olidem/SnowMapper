10ef107da10faccc2acbc4c291af5b7f46b50e7e
Olivier Deman <olideman@gmail.com>
2019-11-26 15:07:51 +0000

    Group controllers improved

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -11,7 +11,15 @@ def new
 end
 
 def create
-
+  @meeting = Meeting.new(group_params)
+  @resort = Resort.find(params[:resort_id])
+  @group.resort = @resort
+  if @group.save!
+    Membership.create(user: current_user, group: @group, admin: true)
+    redirect_to groups_path
+  else
+    render :new
+  end
 end
 
 # def edit
