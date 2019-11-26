068bd90a36c51a155de95693c9a9642448dab8b1
Rob Pickard <rob.pickard11@gmail.com>
2019-11-26 10:54:49 +0000

    Devise registration controller edited to allow setting of resort

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -1,10 +1,17 @@
 class ResortsController < ApplicationController
+  before_action :set_resort, only: %i[show]
+
   def index
     @resorts = Resort.all
   end
 
   def show
-    @resort = Resort.find(params[:id])
     @groups = @resort.groups
+  end
+
+  private
+
+  def set_resort
+    @resort = Resort.find(params[:id])
   end
 end
