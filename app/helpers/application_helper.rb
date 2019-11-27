eb7b8e39a83e3e85ca2fea2e48b6608dd3e5983a
Rob Pickard <rob.pickard11@gmail.com>
2019-11-27 14:18:30 +0000

    Time since last message functionality added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,16 @@ module ApplicationHelper
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
   end
+
+  # Custom helpers
+  def time_since(record)
+    time_diff_mins = (Time.new - record.created_at).fdiv(1.minute).round
+    if time_diff_mins < 60
+      "#{time_diff_mins} minutes ago"
+    elsif time_diff_mins.fdiv(60) < 24
+      "#{time_diff_mins.fdiv(60).round} hours ago"
+    else
+      "#{time_diff_mins.fdiv(60).fdiv(24).round} days ago"
+    end
+  end
 end
