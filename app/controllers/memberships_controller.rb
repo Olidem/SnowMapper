4721ee6d09cc6ca3effa1a08c1fa663438984661
Olivier Deman <olideman@gmail.com>
2019-11-27 14:23:57 +0000

    Few changes added

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,13 +1,11 @@
 class MembershipsController < ApplicationController
-  # before_action :set_group, only: %i[show edit update destroy]
+  before_action :set_membership, only: %i[update destroy]
 
   def user_memberships #same as index but only for memberships you are in.
     @memberships = Membership.all
   end
 
-   def new() end
-
-   def create
+  def create
     @group = Group.find(params[:group_id])
     @membership = Membership.new
     @membership.group = @group
@@ -17,21 +15,17 @@ class MembershipsController < ApplicationController
     else
       redirect_to resort_path(@group.resort)
     end
-   end
+  end
 
-  # # def edit
-
-  # # end
-
-  def update #make admin or not.
-    @membership = Membership.find(params[:id])
+  # make admin or not.
+  def update
     @group = @membership.group
     @membership.update(membership_params)
     redirect_to edit_group_path(@group)
   end
 
-  def destroy #remove the member from the group.
-    @membership = Membership.find(params[:id])
+  # remove the member from the group.
+  def destroy
     @group = @membership.group
     @membership.destroy
     redirect_to edit_group_path(@group)
@@ -43,7 +37,7 @@ class MembershipsController < ApplicationController
     params.require(:membership).permit(:admin)
   end
 
-  # def set_membersip
-  #   @membership = Membership.find(params[:id])
-  # end
+  def set_membership
+    @membership = Membership.find(params[:id])
+  end
 end
