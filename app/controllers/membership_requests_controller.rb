66fb6625cd0af32e47cf9e6914d0728a5b57ec05
Rob Pickard <rob.pickard11@gmail.com>
2019-12-02 17:51:27 +0000

    Bug fixes complete

--- app/controllers/membership_requests_controller.rb
+++ app/controllers/membership_requests_controller.rb
@@ -1,7 +1,7 @@
 class MembershipRequestsController < ApplicationController
   def create
     @group = Group.find(params[:group_id])
-    @groups = @group.resort.groups
+    @groups = params[:membership_request][:groups].split(" ").map { |id| Group.find(id.to_i) }
     @membership_request = MembershipRequest.new(set_params)
     @membership_request.group = @group
     @membership_request.user = current_user
