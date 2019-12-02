028258575d212d3f1bc6590c48880293e3bc8df9
Rob Pickard <rob.pickard11@gmail.com>
2019-12-02 11:02:05 +0000

    Ajax added for resorts sorting method

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -2,13 +2,13 @@ class ResortsController < ApplicationController
   before_action :set_resort, only: %i[show add_user sort_user_count sort_group_created sort_latest_message]
   before_action :set_groups, only: %i[show add_user sort_user_count sort_group_created sort_latest_message]
   before_action :set_empty_group, only: %i[show sort_user_count sort_group_created sort_latest_message]
+  before_action :set_empty_membership_request, only: %i[show sort_user_count sort_group_created sort_latest_message]
 
   def index
     @resorts = Resort.all
   end
 
   def show
-    @membership_request = MembershipRequest.new
   end
 
   def add_user
@@ -19,17 +19,26 @@ class ResortsController < ApplicationController
   # Ordering methods
   def sort_user_count
     @groups = (@groups.sort_by { |group| group.users.count }).reverse
-    render :show
+    respond_to do |format|
+      format.html { render :show }
+      format.js
+    end
   end
 
   def sort_group_created
     @groups = (@groups.sort_by { |group| group.created_at }).reverse
-    render :show
+    respond_to do |format|
+      format.html { render :show }
+      format.js
+    end
   end
 
   def sort_latest_message
     @groups = (@groups.sort_by { |group| group.messages.any? ? group.messages.last.created_at : Time.new(1) }).reverse
-    render :show
+    respond_to do |format|
+      format.html { render :show }
+      format.js
+    end
   end
 
   private
@@ -44,5 +53,9 @@ class ResortsController < ApplicationController
 
   def set_empty_group
     @group = Group.new
+  end
+
+  def set_empty_membership_request
+    @membership_request = MembershipRequest.new
   end
 end
